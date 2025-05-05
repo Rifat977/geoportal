a11f5281e98de52a27d5008629b530ff9920c36a
rifat977 <itscrifat5147@gmail.com>
2025-05-06 00:01:41 +0600

    Phase 2: RESTful API Development

---
+++ core/urls.py
@@ -0,0 +1,34 @@
+from django.urls import path
+from .views import CountryViewSet
+
+country_list = CountryViewSet.as_view({
+    'get': 'list',
+    'post': 'create'
+})
+
+country_detail = CountryViewSet.as_view({
+    'get': 'retrieve',
+    'put': 'update',
+    'patch': 'partial_update',
+    'delete': 'destroy'
+})
+
+same_region = CountryViewSet.as_view({
+    'get': 'same_region'
+})
+
+by_language = CountryViewSet.as_view({
+    'get': 'by_language'
+})
+
+search = CountryViewSet.as_view({
+    'get': 'search'
+})
+
+urlpatterns = [
+    path('', country_list, name='country-list'),
+    path('<int:pk>/', country_detail, name='country-detail'),
+    path('<int:pk>/same-region/', same_region, name='country-same-region'),
+    path('language/<str:lang_code>/', by_language, name='country-by-language'),
+    path('search/', search, name='country-search'), 
+]
--- core/views.py
+++ core/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from rest_framework import viewsets, filters
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from .models import Country
+from .serializers import CountrySerializer
+from rest_framework import status
 
-# Create your views here.
+
+class CountryViewSet(viewsets.ModelViewSet):
+    queryset = Country.objects.all()
+    serializer_class = CountrySerializer
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['name_common', 'name_official']
+
+    @action(detail=True, methods=['get'])
+    def same_region(self, request, pk=None):
+        country = self.get_object()
+        regional_countries = Country.objects.filter(region=country.region).exclude(pk=country.pk)
+        serializer = self.get_serializer(regional_countries, many=True)
+        return Response(serializer.data)
+
+    @action(detail=False, methods=['get'], url_path='language/(?P<lang_code>[a-z]{3})')
+    def by_language(self, request, lang_code=None):
+        countries = Country.objects.filter(languages__has_key=lang_code)
+        serializer = self.get_serializer(countries, many=True)
+        return Response(serializer.data)
+    
+    @action(detail=False, methods=['get'], url_path='search')
+    def search(self, request):
+        query = request.query_params.get('search', '')
+        if not query:
+            return Response({'detail': "Query param is required."}, status=400)
+        results = self.queryset.filter(name_common__icontains=query)
+        serializer = self.get_serializer(results, many=True)
+        return Response(serializer.data)
